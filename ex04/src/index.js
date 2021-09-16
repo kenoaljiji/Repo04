9c72211b9ed18f6a3f76b9fd5533f8589a3e944d
kenoaljiji <studioaljiji@gmail.com>
2021-09-16 20:18:24 +0200

    decimal numbers

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+  // only change code below this line
+  var myDecimal = 7.3;
+  var myDecimalE = 345e-5;
+
+  // Only change code above this line
+
+  return {
+    myDecimal,
+    myDecimalE,
+  };
+}
+
+console.log(main());
+
+module.exports = main;
