6e42e88cae116c3f27083db75b999d2c95af880c
kenoaljiji <studioaljiji@gmail.com>
2021-09-16 23:00:24 +0200

    assing  a string literal value to variables

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+  // only change code below this line
+
+  var myFirstName = "Kenan";
+  var myLastName = "Aljiji";
+  var myHomeTown = "Cazin";
+
+  // Only change code above this line
+
+  return {
+    myFirstName,
+    myLastName,
+    myHomeTown,
+  };
+}
+console.log(main());
+
+module.exports = main;
