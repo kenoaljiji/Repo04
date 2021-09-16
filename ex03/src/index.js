b94a839397d7627ce5c8706fce24f45100551385
kenoaljiji <studioaljiji@gmail.com>
2021-09-16 20:13:21 +0200

    decrement number

---
+++ ex03/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+  var myRes = 15;
+
+  // Decrement with --
+
+  myRes--;
+
+  // Only change code above this line
+
+  return myRes;
+}
+
+console.log(main());
+
+module.exports = main;
