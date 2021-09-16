d696a1e33c14498eefa247668737f278e5c3ce8a
kenoaljiji <studioaljiji@gmail.com>
2021-09-16 20:09:54 +0200

    increment number

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+  var myRes = 76;
+  // increment with ++
+
+  myRes++;
+
+  // Only change code above this line
+
+  return myRes;
+}
+
+console.log(main());
+
+module.exports = main;
