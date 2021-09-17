2996c8d3de443e0a88bd70768c739138f05d67b2
kenoaljiji <studioaljiji@gmail.com>
2021-09-17 09:49:25 +0200

    change the value of decimals number

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,7 +1,7 @@
 function main() {
   // only change code below this line
-  var myDecimal = 7.3;
-  var myDecimalE = 345e-5;
+  var myDecimal = 8.2;
+  var myDecimalE = 254e-5;
 
   // Only change code above this line
 
